4959d8bde807c0d4efcee6eec20d65501f9284a1
lalitagrawal2004 <lalit.study.1@gmail.com>
2024-05-10 15:43:12 +0530

    Add files via upload

---
+++ data.php
@@ -0,0 +1,42 @@
+<?php
+// Function to prompt user for input
+function prompt($prompt_msg){
+    echo($prompt_msg . " ");
+    $input = rtrim(fgets(STDIN), "\n");
+    return $input;
+}
+
+// Get age and nationality from user input
+$age = prompt("Enter your age:");
+$nationality = prompt("Enter your nationality:");
+
+// Applying conditions on nationality and age
+if ($nationality == "Indian") {
+    if ($age >= 18) {
+        echo "Eligible to give vote";
+    } else {
+        echo "Not eligible to give vote";
+    }
+}
+?>
+How quickly can I expect to see results from SEM campaigns?
+Answer: The timeframe for seeing results from SEM campaigns can vary based on factors such as campaign goals, competition, and budget. Typically, you may start to see initial results within a few days to a couple of weeks, with improvements over time through ongoing optimization efforts.
+
+
+
+What futuristic trends do you foresee in SEM?
+Answer: As AI and machine learning continue to advance, we expect SEM to become even more personalized and predictive, with a greater emphasis on automation and real-time bidding strategies.
+
+
+How much control will I have over my SEM campaigns?
+Answer: You'll have complete control over objectives, budget, and messaging. We'll collaborate closely to ensure your campaigns align with your goals and preferences.
+
+
+
+Will SEM help my business stand out among competitors in search results?
+Answer: Absolutely! SEM enhances your visibility in search results, ensuring your business appears prominently to potential customers, often ahead of competitors, driving more clicks and conversions.
+
+
+
+What measures ensure efficient budget spending in SEM campaigns?
+Answer: We employ data-driven strategies and constant monitoring to optimize your budget spending in SEM campaigns, ensuring each dollar spent delivers maximum return on investment (ROI) and drives towards your campaign objectives.
---
+++ demo.php
@@ -0,0 +1,12 @@
+<html>
+<body>
+<?php
+function add_five(&$value) {
+ $value += 5;
+}
+$num = 2;
+add_five($num);
+echo $num;
+?>
+</body>
+</html>
